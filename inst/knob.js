814e94444546c74b57aafa6109870a2a0663b0a9
Joe Cheng <joe@rstudio.com>
2014-07-24 16:05:38 -0700

    Unified binding

--- inst/knob.js
+++ inst/knob.js
@@ -1,21 +1,10 @@
-if (window.Shiny == undefined){
-  renderKnob('#' + payload.id, payload)
-} else {
-// This output binding handles statusOutputBindings
-  var knobOutputBinding = new Shiny.OutputBinding();
-  $.extend(knobOutputBinding, {
-    find: function(scope) {
-      return scope.find('.knob_output');
-    },
-    renderValue: renderKnob
-  });
-  Shiny.outputBindings.register(knobOutputBinding, 'dashboard.knobOutputBinding');
-}
-
-function renderKnob(el, data){
-  if (!$(el).val()){
-    $(el).knob()
+HTMLWidgets.widget({
+  name: "knob",
+  initialize: function(el) {
+    $(el).knob();
+  },
+  renderValue: function(el, data) {
+    $(el).trigger("configure", data);
+    $(el).val(data.value).trigger("change");
   }
-  $(el).val(data.value).trigger('change')
-  $(el).trigger('configure', data)
-}
+});
